935121cca078264a37b2e7a1208b6374c9e4bf30
Mikhail Podolskiy <Edge769@yandex.ru>
2020-06-28 01:44:28 +0300

    add exact command parsing tests

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -8,3 +8,8 @@ spec = do
   describe "parseCommand" $ do
     it "doesn't parse invalid command" $ do
       parseCommand "I'am an invalid one" `shouldBe` Nothing
+
+    it "parses exact commands" $ do
+      parseCommand "L" `shouldBe` Just TurnLeft
+      parseCommand "R" `shouldBe` Just TurnRight
+      parseCommand "F" `shouldBe` Just MoveForward
