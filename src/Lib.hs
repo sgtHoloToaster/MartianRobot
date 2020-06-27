f09747f8eb325659fa45e77481d0e25dc726992d
Mikhail Podolskiy <Edge769@yandex.ru>
2020-06-28 01:49:30 +0300

    add exact commands parsing implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,4 +10,7 @@ data Command =
   deriving (Show, Eq)
 
 parseCommand :: String -> Maybe Command
-parseCommand str = Nothing
+parseCommand "L" = Just TurnLeft
+parseCommand "R" = Just TurnRight
+parseCommand "F" = Just MoveForward
+parseCommand _ = Nothing
